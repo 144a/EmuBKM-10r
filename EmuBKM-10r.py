c488562ec0275833f32e8d6309c0e0189f0fc6d7
Andy Gatza <andy.gatza@gmail.com>
2024-01-02 15:19:38 -0600

    Moved to a central class structure -Corrected Issue with shift functionality -Fully usuable as a python module

--- EmuBKM-10r.py
+++ EmuBKM-10r.py
@@ -2,7 +2,6 @@ import serial
 import io
 import time
 
-
 # Sony BKM-10r Serial Protocol
 # Link to info: https://pastebin.com/aTUWf33J
 
@@ -72,23 +71,23 @@ HUMAN_READABLE_COMMANDS = {
 	"IMT":["COMMAND", "IMT"],
 	"Shift":["COMMAND", "SHIFT"],
 	"Overscan":["COMMAND", "OVERSCAN_16_9"],
-	"16:9":["COMMAND", "SHIFT", "OVERSCAN_16_9"],
+	"16:9":["COMMAND", "SHIFT", "OVERSCAN_16_9", "SHIFT"],
 	"HorizSync":["COMMAND", "HORIZSYNC_SYNC"],
-	"Sync":["COMMAND", "SHIFT", "HORIZSYNC_SYNC"],
+	"Sync":["COMMAND", "SHIFT", "HORIZSYNC_SYNC", "SHIFT"],
 	"VertSync":["COMMAND", "VERTSYNC_BLUEONLY"],
-	"BlueOnly":["COMMAND", "SHIFT", "VERTSYNC_BLUEONLY"],
+	"BlueOnly":["COMMAND", "SHIFT", "VERTSYNC_BLUEONLY", "SHIFT"],
 	"Mono":["COMMAND", "MONO_RED"],
-	"Red":["COMMAND", "SHIFT", "MONO_RED"],
+	"Red":["COMMAND", "SHIFT", "MONO_RED", "SHIFT"],
 	"Aperature":["COMMAND", "APT_GREEN"],
-	"Green":["COMMAND", "SHIFT", "APT_GREEN"],
+	"Green":["COMMAND", "SHIFT", "APT_GREEN", "SHIFT"],
 	"Comb":["COMMAND", "COMB_BLUE"],
-	"Blue":["COMMAND", "SHIFT", "COMB_BLUE"],
+	"Blue":["COMMAND", "SHIFT", "COMB_BLUE", "SHIFT"],
 	"F1":["COMMAND", "F1_F3"],
-	"F3":["COMMAND", "SHIFT", "F1_F3"],
+	"F3":["COMMAND", "SHIFT", "F1_F3", "SHIFT"],
 	"F2":["COMMAND", "F2_F4"],
-	"F4":["COMMAND", "SHIFT", "F2_F4"],
+	"F4":["COMMAND", "SHIFT", "F2_F4", "SHIFT"],
 	"SafeArea":["COMMAND", "SAFEAREA_ADDR"],
-	"Address":["COMMAND", "SHIFT", "SAFEAREA_ADDR"],
+	"Address":["COMMAND", "SHIFT", "SAFEAREA_ADDR", "SHIFT"],
 	"Up":["COMMAND", "UP"],
 	"Down":["COMMAND", "DOWN"],
 	"Enter":["COMMAND", "ENTER"],
@@ -112,125 +111,153 @@ HUMAN_READABLE_COMMANDS = {
 	"UpdateChannelName":["SCRIPT", "CHANNEL_NAME"]
 }
 
-#--------------------Custom Functions---------------------
-# These are made using the byte commands implemented above
-# These are NOT standard to the BKM series of controllers
+class EmuBKM10r:
+	ser = None
+	def __init__(self, serial_port, baudrate = 38400):
+		# Open Serial Port
+		self.ser = serial.Serial()
+		self.ser.baudrate = baudrate
+		self.ser.port = serial_port
 
-def writeCommand(command, skipISW=False):
-	""" Sends correct byte array for corresponding command """
-	if skipISW:
-		ser.write(bytearray(COMMANDS["ISW"]))
-	ser.write(bytearray(COMMANDS[command]))
-	ser.flush()
-	if command == "MENU" or command == "Power":	
-		time.sleep(0.50)
-	else:
-		time.sleep(0.025)
-	print(command)
+	def connect(self):
+		try:
+			self.ser.open()
+		except:
+			print ("Error connecting to Monitor")
+	
+	def close(self):
+		"""Closes serial connection to Monitor"""
+		self.ser.close()
+	
+	def flush(self):
+		"""Flushes all current serial data"""
+		self.ser.flush()
 
-def repeatCommand(command, reps, skipISW=False):
-	""" Repeats COMMANDS N times with a 0.05 delay between """
-	for i in range(reps):
-		writeCommand(command, skipISW)
+	def writeCommand(self, command, skipISW=False):
+		""" Sends correct byte array for corresponding command """
+		if skipISW:
+			self.ser.write(bytearray(COMMANDS["ISW"]))
+		self.ser.write(bytearray(COMMANDS[command]))
+		self.ser.flush()
+		if command == "MENU" or command == "POWER":	
+			time.sleep(0.50)
+		elif command == "SHIFT":
+			self.ser.write(bytearray(COMMANDS["ISW"]))
+			time.sleep(0.2)
+			self.ser.flush()
+			self.ser.write(bytearray(COMMANDS["ISW"]))
+		else:
+			time.sleep(0.025)
+		#print(command)
 
-# Function to enter text whenever applicable
-def writeText():
-	dif = input("Input Text: ")
-	for s in dif:
-		dir = 1
-		charstr = "abcdefghijklmnopqrstuvwxyz0123456789():;.-+/& "
-		n = charstr.index(s.lower()) + 1
-		
-		if n > len(charstr) // 2:
-			dir = -1
-			n = len(charstr) - (n - 1)
-		print(n)
-		for i in range(n):
-			if dir == 1:
-				writeCommand("UP")
-			else:
-				writeCommand("DOWN")
-		writeCommand("ENTER")
-		ser.flush()
-		time.sleep(.1)
-	writeCommand("ENTER")
-	ser.flush()
+	def repeatCommand(self, command, reps, skipISW=False):
+		""" Repeats COMMANDS N times with a 0.05 delay between """
+		for i in range(reps):
+			self.writeCommand(command, skipISW)
 
-def updateChannelName():
-	# Call for Menu
-	writeCommand("MENU")
-	time.sleep(0.1)
-
-	# Select Channel Input
-	# Move to Name function
-	repeatCommand("DOWN", 2, skipISW=True)
-	writeCommand("ENTER")
-	time.sleep(0.1)
-
-	# Get channel to change name
-	num = int(input("Channel Number to Update: "))
-	channel_num = "NUM" + str(num)
-
-	writeCommand(channel_num)
-	time.sleep(3)
-
-	# Move to Name function
-	repeatCommand("DOWN", 6, skipISW=True)
-
-	writeCommand("ENTER")
-	time.sleep(0.12)
-
-	writeCommand("UP")
-
-	writeCommand("ENTER")
-	time.sleep(0.1)
-
-	writeText()
-
-# Writes bytes for each command
-def sendCommand(inp_str):
-	try:
-		command_list = HUMAN_READABLE_COMMANDS[inp_str].copy()
-	except:
-		print("Not a Valid Command")
-		return 0
-
-	command_type = command_list.pop(0)
-	if command_type == "COMMAND":
-		for command in command_list:
-			writeCommand(command)
-		return 1
-
-	#--------------------Encoders---------------------
-
-	# Encoder input requires switching banks to IEN
-	# After, it accepts an input in the form of
-	# <0x44, 'encoder id', 'number of ticks'>
+	#--------------------Custom Functions---------------------
+	# These are made using the byte commands implemented above
+	# These are NOT standard to the BKM series of controllers
 
 
-	# When calling an encoder command, it will prompt
-	# for user input. The number is a two's compliment byte,
-	# and as such you can turn the knob to the right (positive input)
-	# by using a number less than 128, while you can turn the knob
-	# to the left by using a number greater than 128 but less than 256
+	# Function to enter text whenever applicable
+	def writeText(self):
+		dif = input("Input Text: ")
+		for s in dif:
+			dir = 1
+			charstr = "abcdefghijklmnopqrstuvwxyz0123456789():;.-+/& "
+			n = charstr.index(s.lower()) + 1
+			
+			if n > len(charstr) // 2:
+				dir = -1
+				n = len(charstr) - (n - 1)
+			print(n)
+			for i in range(n):
+				if dir == 1:
+					self.writeCommand("UP")
+				else:
+					self.writeCommand("DOWN")
+			self.writeCommand("ENTER")
+			time.sleep(.1)
+			self.ser.flush()
+		self.writeCommand("ENTER")
+		self.ser.flush()
 
+	def updateChannelName(self):
+		# Call for Menu
+		self.writeCommand("MENU")
+		time.sleep(0.1)
 
-	#if inp_str == "PhaseInc":
-	#	dif = int(input("Input Wanted Difference:")) # Ask for user input
-	#	bytes = [PHASE_ENC[0], PHASE_ENC[1], dif * 4] # Create array with correct format
-	#	ser.write(bytearray(IEN)) # Switch to encoder bank
-	#	ser.flush()
-	#	ser.write(bytearray(bytes)) # Write byte array
-	#	ser.flush()
-	#	ser.write(bytearray(ISW)) # Switch back to key bank
-	#	return 1
-	if command_type == "SCRIPT":
-		if command_list[0] == "CHANNEL_NAME":
-			updateChannelName()
+		# Select Channel Input
+		# Move to Name function
+		self.repeatCommand("DOWN", 2, skipISW=True)
+		self.writeCommand("ENTER")
+		time.sleep(0.1)
+
+		# Get channel to change name
+		num = int(input("Channel Number to Update: "))
+		channel_num = "NUM" + str(num)
+
+		self.writeCommand(channel_num)
+		time.sleep(3)
+
+		# Move to Name function
+		self.repeatCommand("DOWN", 6, skipISW=True)
+
+		self.writeCommand("ENTER")
+		time.sleep(0.12)
+
+		self.writeCommand("UP")
+
+		self.writeCommand("ENTER")
+		time.sleep(0.1)
+
+		self.writeText()
+
+	# Writes bytes for each command
+	def sendCommand(self, inp_str):
+		try:
+			command_list = HUMAN_READABLE_COMMANDS[inp_str].copy()
+		except:
+			print("Not a Valid Command")
+			return 0
+
+		command_type = command_list.pop(0)
+		if command_type == "COMMAND":
+			for command in command_list:
+				self.writeCommand(command)
 			return 1
 
-	# If the command does not fit any currently supported command, return 0
-	return 0
+		#--------------------Encoders---------------------
+
+		# Encoder input requires switching banks to IEN
+		# After, it accepts an input in the form of
+		# <0x44, 'encoder id', 'number of ticks'>
+
+
+		# When calling an encoder command, it will prompt
+		# for user input. The number is a two's compliment byte,
+		# and as such you can turn the knob to the right (positive input)
+		# by using a number less than 128, while you can turn the knob
+		# to the left by using a number greater than 128 but less than 256
+
+
+		#if inp_str == "PhaseInc":
+		#	dif = int(input("Input Wanted Difference:")) # Ask for user input
+		#	bytes = [PHASE_ENC[0], PHASE_ENC[1], dif * 4] # Create array with correct format
+		#	ser.write(bytearray(IEN)) # Switch to encoder bank
+		#	ser.flush()
+		#	ser.write(bytearray(bytes)) # Write byte array
+		#	ser.flush()
+		#	ser.write(bytearray(ISW)) # Switch back to key bank
+		#	return 1
+		if command_type == "SCRIPT":
+			if command_list[0] == "CHANNEL_NAME":
+				self.updateChannelName()
+				return 1
+
+		# If the command does not fit any currently supported command, return 0
+		return 0
 
 
 if __name__ == "__main__":
@@ -238,16 +265,11 @@ if __name__ == "__main__":
 	print("Sony BKM-10r Emulated Controller")
 	print("Type 'help' for Info and 'exit' to Quit")
 
-	# Open Serial Port
-	ser = serial.Serial()
-	ser.baudrate = 38400
-
 	#ser.port = input("Enter name of device (ex. '/dev/ttyUSB0' for linux or 'COM3' for windows): ")
 
-	#ser.port = '/dev/ttyUSB0'
-	ser.port = 'COM9'
-	# Open Port
-	ser.open()
+	bkm = EmuBKM10r('COM9')
+
+	bkm.connect() # opens serial port and connects to monitor
 
 	inp = ""
 
@@ -257,11 +279,11 @@ if __name__ == "__main__":
 			# Create help command response
 			break
 		if inp != "exit":
-			ret = sendCommand(inp) # Parse Input and send the proper write() commands
+			ret = bkm.sendCommand(inp) # Parse Input and send the proper write() commands
 			if ret == 1:
-				ser.flush() # Need one final Flush to send last write()
+				bkm.flush() # Need one final Flush to send last write()
 				print("Command Successfully Sent")
 			else:
 				print("Command Failed")
 		#print(ser.read(3))
-	ser.close()
+	bkm.close()
